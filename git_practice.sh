ef81b3774282cef3f0cb9bc797d7ca8b46724440
SnrDevopsEng1 <seremm254@gmail.com>
2023-09-25 23:15:29 -0400

    jenkins

---
+++ git_practice.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# 
+#
+# read -p "What is your name? " NAME
+#
+# read -p "Are you taking the utrains class?" c
+#
+#  
+#
+#  if [ $c = yes ]
+#
+#  then 
+#
+#    echo "Good job $NAME"
+#
+#    else 
+#
+#      echo "Please check the website (utrain.sorg)"
+#
+#      fi
+#
+#      #echo "Your name is $NAME and your answer is $c to the utrains class taking"
