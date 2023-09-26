dbd2fbb616c40ac14dbda0a845b03a1d6e5704a9
SnrDevopsEng1 <seremm254@gmail.com>
2023-09-25 23:25:40 -0400

    new addition

--- git_practice.sh
+++ git_practice.sh
@@ -21,3 +21,38 @@
 #      fi
 #
 #      #echo "Your name is $NAME and your answer is $c to the utrains class taking"
+#
+#      If statement
+#      #!/bin/bash
+#
+#       
+#
+#        if [  -f /etc/passwd ]
+#
+#        then
+#
+#         echo exist
+#
+#         else 
+#
+#          echo Missing
+#
+#          fi
+#
+#          if [ -d /tmp/serge ] 
+#
+#           then 
+#
+#              echo exist
+#
+#              else
+#
+#                 mkdir /tmp/serge
+#
+#                 echo new > /tmp/serg/file
+#
+#                 cat /tmp/serge/file
+#
+#                  
+#
+#                  fi
